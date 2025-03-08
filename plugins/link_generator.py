04c25addc89ba2df36677d02c1abcb939167b910
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2025-03-09 02:12:24 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -34,7 +34,7 @@ async def batch(client: Client, message: Message):
 
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
-    link = f"https://the4xteam.in/botserver{client.username}?start={base64_string}"
+    link = f"https://the4xteam.in/botserver?start={base64_string}"
     # Create inline keyboard markup
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://the4xteam.in/share/url?url={link}')]])
     # Send the link with markup
@@ -55,6 +55,6 @@ async def link_generator(client: Client, message: Message):
             continue
 
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
-    link = f"https://the4xteam.in/botserver{client.username}?start={base64_string}"
+    link = f"https://the4xteam.in/botserver?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://the4xteam.in/share/url?url={link}')]])
     await channel_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
